5ee775e942dbc513023d489b0ba0a164a05c81d4
Lucia Sandoval <asandovalp9@miumg.edu.gt>
2024-05-26 11:26:41 -0600

    Commit Calificacion

---
+++ ProyectoI/Clases/Calificacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoI.Clases
+{
+    internal class Calificacion
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public string CursoId { get; set; }
+        public double Nota { get; set; }
+        public string Retroalimentacion { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
---
+++ ProyectoI/Clases/CalificacionDAO.cs
@@ -0,0 +1,89 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoI.Clases
+{
+    // internal class CalificacionDAO
+
+    internal class CalificacionDAO
+    {
+        private string connectionString = "server=127.0.0.1;" +
+                                          "user=root;" +
+                                          "pwd=aguapura02;" +
+                                          "database=usuarios;";
+
+        public void AsignarCalificacion(Calificacion calificacion)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "INSERT INTO calificaciones (usuario_id, curso_id, nota, retroalimentacion, fecha) " +
+                               "VALUES (@UsuarioId, @CursoId, @Nota, @Retroalimentacion, @Fecha)";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@UsuarioId", calificacion.UsuarioId);
+                    cmd.Parameters.AddWithValue("@CursoId", calificacion.CursoId);
+                    cmd.Parameters.AddWithValue("@Nota", calificacion.Nota);
+                    cmd.Parameters.AddWithValue("@Retroalimentacion", calificacion.Retroalimentacion);
+                    cmd.Parameters.AddWithValue("@Fecha", calificacion.Fecha);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<Calificacion> ObtenerCalificaciones(int usuarioId)
+        {
+            var calificaciones = new List<Calificacion>();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM calificaciones WHERE usuario_id = @UsuarioId";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@UsuarioId", usuarioId);
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var calificacion = new Calificacion
+                            {
+                                Id = reader.GetInt32("id"),
+                                UsuarioId = reader.GetInt32("usuario_id"),
+                                //CursoId = reader.GetInt32("curso_id"),
+                                Nota = reader.GetDouble("nota"),
+                                Retroalimentacion = reader.GetString("retroalimentacion"),
+                                Fecha = reader.GetDateTime("fecha")
+                            };
+                            calificaciones.Add(calificacion);
+                        }
+                    }
+                }
+            }
+
+            return calificaciones;
+        }
+
+        public void ActualizarCalificacion(Calificacion calificacion)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "UPDATE calificaciones SET nota = @Nota, retroalimentacion = @Retroalimentacion, fecha = @Fecha " +
+                               "WHERE id = @Id";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Nota", calificacion.Nota);
+                    cmd.Parameters.AddWithValue("@Retroalimentacion", calificacion.Retroalimentacion);
+                    cmd.Parameters.AddWithValue("@Fecha", calificacion.Fecha);
+                    cmd.Parameters.AddWithValue("@Id", calificacion.Id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
